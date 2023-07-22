2747255078d3dc54a0ad310065e4b45e4cddbfc5
morituriblog <50313881+morituriblog@users.noreply.github.com>
2023-07-22 14:01:11 +0900

    Add dialog_core.ps1

---
+++ dialog_core.ps1
@@ -0,0 +1,34 @@
+﻿Add-Type -AssemblyName PresentationFramework
+
+function Set-WpfControls {
+    param([hashtable]$ControlHashTable, [string]$XamlText)
+
+    $wpfObj = [System.Windows.Markup.XamlReader]::Parse($XamlText)
+
+    $xnavi = ([xml]$XamlText).CreateNavigator()
+    foreach ($node in $xnavi.Select("//@Name")) {
+        $name = $node.Value
+        $ControlHashTable["$name"] = $wpfObj.FindName($name)
+    }
+}
+
+function Set-MethodObject {
+    param([PSCustomObject]$MethodObject, [hashtable]$MethodHashtable)
+
+    foreach ($name in $methodHashtable.Keys) {
+        $MethodObject | Add-Member -MemberType ScriptMethod -Name $name -Value $methodHashtable[$name]
+    }
+}
+
+function Set-ControlEvent {
+    param([hashtable]$Controls, [hashtable]$EventHashtable)
+
+    foreach ($key in $EventHashtable.Keys) {
+        $controlAndEvent = $key -split "_"
+        $controlName = $controlAndEvent[0]
+        $eventName = $controlAndEvent[1]
+
+        $addEvent = Invoke-Expression -Command ('$Controls[$controlName].Add_' + $eventName)
+        $addEvent.Invoke($EventHashtable[$key])
+    }
+}
